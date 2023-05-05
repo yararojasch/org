9c67ab1b9a95abe8a453e994f81671a1de503cc1
yara <yara.rojas.ch@gmail.com>
2023-05-05 15:57:01 -0400

    Clase 06

---
+++ src/componentes/ListaOpciones/index.js
@@ -0,0 +1,23 @@
+import "./ListaOpciones.css"
+
+const ListaOpciones = (props) => {
+
+    //Método Map -> arreglo.map( () => {
+    //    return <option></option>
+    //})
+ 
+    const manejarCambio = (e) => {
+        console.log("cambio", e.target.value)
+        props.actualizarEquipo(e.target.value)
+    }
+
+    return <div className="lista-opciones">
+        <label>Equipos</label>
+        <select value={props.valor} onChange={manejarCambio}>
+            <option value="" disabled defaultValue="" hidden>Seleccionar equipo</option>
+           {props.equipos.map((equipo, index) => <option key={index} value={equipo}>{equipo}</option>)}
+        </select>
+    </div>
+}
+
+export default ListaOpciones
---
+++ src/componentes/MiOrg/index.js
@@ -0,0 +1,18 @@
+import { useState } from "react"
+import "./MiOrg.css"
+
+const MiOrg = (props) => {
+    //como usar useState: const [nombreVariable, funcionQueActualiza] = useState(valorInicial)
+    //const [mostrar, actualizarMostrar] = useState(true)
+    //const manejarClick = () => {
+      //  console.log("Mostrar/Ocultar elemento", !mostrar)
+        //actualizarMostrar(!mostrar)
+    //}
+
+    return <section className="orgSection">
+        <h3 className="title">Mi organización </h3>
+        <img src="/img/add.png" alt="add" onClick={props.cambiarMostrar}/>
+    </section>
+}
+
+export default MiOrg
